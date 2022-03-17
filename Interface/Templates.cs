bdc909f33c3138899c60e01e658cc2491874d38a
scalar-ROG <78349301+scalar-tns@users.noreply.github.com>
2022-03-17 13:10:14 +0100

    Added interface templates

--- Interface/Templates.cs
+++ Interface/Templates.cs
@@ -3,33 +3,58 @@
     // TODO:    Template for each entity
     public static class Templates
     {
-        public static void _RootCommand(
+        public static string _RootCommand(
             string title,
             string description,
             bool setverbosity)
         {
+            return
+                $@"RootCommand command = Constructors.MakeRootCommand(
+                title: {title},
+                description: {description},
+                setverbosity: {setverbosity});";
         }
 
-        public static void _Command(
+        public static string _Command(
+            string name,
             Command command,
             string symbol,
-            string? defaultvalue,
             string description,
             bool setverbosity)
         {
+            return
+        @$"public static Command _{name}(RootCommand {command})
+        {{
+            return Constructors.MakeCommand(
+                command: {command},
+                symbol: {symbol},
+                description: {description},
+                setverbosity: {setverbosity});
+        }}";
         }
 
-        public static void _Argument(
+        public static string _Argument(
             Type T,
+            string name,
             Command command,
             string symbol,
-            string? defaultvalue,
+            string defaultvalue,
             string description)
         {
+            return
+        $@"public static Argument<{T}> _{name}(Command {command})
+        {{
+            return Constructors.MakeArgument<{T}>(
+                command: {command},
+                symbol: {symbol},
+                defaultvalue: {defaultvalue},
+                description: {description});
+        }}";
         }
 
-        public static void _Option(
+        public static string _Option(
             Type T,
+            string name,
             Command command,
             bool required,
             string symbol,
@@ -37,6 +62,17 @@
             string? defaultvalue,
             string description)
         {
+            return
+        $@"public static Option<{T}> _{name}(Command {command})
+        {{
+            return Constructors.MakeOption<{T}>(
+                command: {command},
+                required: {required},
+                symbol: {symbol},
+                alias: {alias},
+                defaultvalue: {defaultvalue},
+                description: {description});
+        }}";
         }
     }
 }
--- Parser.cs
+++ Parser.cs
@@ -14,7 +14,7 @@ namespace autocli
         {
             var levelSwitch = new LoggingLevelSwitch(); // .ControlledBy(levelSwitch)
             Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
+                .MinimumLevel.Information()
                 .WriteTo.Console(outputTemplate:
                                 "[{Timestamp:HH:mm:ss:ff} {Level:u4}] {Message:1j}{NewLine}{Exception}")
                 .WriteTo.File("./logs/autocli.log.txt", rollingInterval: RollingInterval.Minute)
@@ -26,30 +26,33 @@ namespace autocli
                 title: "AUTOCLI : automation for CLI applications interface creation",
                 description: "[autocli] aims to automate .NET 6.0.* CLI applications development based on an input architecture stored in a .json file.\nThe configuration file stores the architecture for the project's commands, subcommands, options, arguments and properties.",
                 setverbosity: true);
-            Log.Information("RootCommand built.");
 
             Command creation = Commands._creation(command);
             Command generation = Commands._generation(command);
-            Log.Information("Commands and subcommands built.");
+            Log.Debug("Commands and subcommands built.");
             // ===========================================OPTIONS===========================================
 
             Option<DirectoryInfo> dir_choice = Options._dir_choice(creation);
             Option<string> pushing = Options._pushing(generation);
-            Log.Information("Options built.");
+            Log.Debug("Options built.");
             // ===========================================ARGUMENTS===========================================
 
             Argument<string> file_name = Arguments._file_name(creation);
             Argument<string> file_path = Arguments._file_path(generation);
-            Log.Information("Arguments built.");
+            Log.Debug("Arguments built.");
             // ===========================================HANDLERS===========================================
 
-            Log.Information("Implementing handlers...");
-            creation.SetHandler<string, DirectoryInfo>(Handlers.Create_Template, file_name);
-            generation.SetHandler<string>(Handlers.Generate, file_path);
+            Log.Debug("Implementing handlers...");
+            creation.SetHandler<string, DirectoryInfo>(Handlers.Create_Template,
+                file_name,
+                dir_choice);
+            generation.SetHandler<string>(Handlers.Generate,
+                file_path,
+                pushing);
             // ===========================================INVOKE===========================================
 
             // Parse the incoming args and invoke the handler
-            Log.Information("Invoking args...");
+            Log.Debug("Invoking args...");
             Log.CloseAndFlush();
             await command.InvokeAsync(args);
         }
